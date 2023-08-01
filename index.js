b5ef0758b885a72c70ea381572f66a9bd37d7dc1
shubhamadav <shubhamyad100@gmail.com>
2023-08-01 18:47:45 +0530

    application

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
-const mongoose = require('mongoose');
-mongoose.connect("mongodb://localhost:27017/company");
+//const mongoose = require('mongoose');
+//mongoose.connect("mongodb://localhost:27017/company");
 
+const express = require ('express')
+
+const app = express();
+
+/* app.get('/',function(req,res){
+    res.send('Get Method')
+})
+
+app.all('*',(req,res)=>{
+    res.send('Page not founDynamic Routes Part 1')
+})  */
+
+app.get('/cd',(req , res , next)=>{
+    console.log('first callback')
+    next()
+},
+(req , res)=>{
+    console.log('second callback')
+    res.send('its time to do think roung')
+})
+
+app.listen(3000)
